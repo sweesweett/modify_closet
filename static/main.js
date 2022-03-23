219cf5fb10f82f31a3123c89ef65fe3761eb2543
sweesweett <sweesweet@naver.com>
2022-03-23 15:41:31 +0900

    기본옷장 구상중

--- static/main.js
+++ static/main.js
@@ -1,4 +1,4 @@
-const desc = "옷장을 채워보세요!n다양한 옷들을 카테고리에 맞게 정리하고n기온에 맞는 옷을 추천해 드려요!n";
+const desc = "옷장을 채워보세요!n다양한 옷을 카테고리에 맞게 정리하고n기온에 맞는 옷을 추천해 드려요!n";
 const text = document.querySelector(".desc");
 let i = 0;
 
