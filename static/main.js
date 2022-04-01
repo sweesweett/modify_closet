71b0b4e4958ebfe5a4e7c23e7a835d126a8ebb89
sweesweett <sweesweet@naver.com>
2022-04-01 16:02:08 +0900

    220401 설명말풍선만들기

--- static/main.js
+++ static/main.js
@@ -1,18 +1,12 @@
 const desc = "옷장을 채워보세요!n다양한 옷을 카테고리에 맞게 정리하고n기온에 맞는 옷을 추천해 드려요!n";
 const text = document.querySelector(".desc");
 let i = 0;
-
+let txt = desc.split('n')
 function typingDesc(){
-    if (i < desc.length) {
-    let txt = desc.charAt(i);
-    if(txt==='n'){
-      text.innerHTML +='<br>';
+    if (i < txt.length) {
+      text.innerHTML +=`${txt[i]}<br>`;
     }
-    else{
-      text.innerHTML += txt
-    }
-    
+        
     i++;
-    }
 }
-setInterval(typingDesc, 80)
+setInterval(typingDesc, 500)
